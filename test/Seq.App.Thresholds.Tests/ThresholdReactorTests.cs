48594ee9c3f4d0b1b4b73852a7ab852fcb2fa459
Ben Brandt <bbrandt@users.noreply.github.com>
2015-11-05 17:09:25 -0600

    Add failing test when_threshold_exceeded_only_one_message_should_be_logged
    Borrowed some code from Kevin Lewis' unit tests for EmailReactorTests.cs
    Current behavior is to log each time an event is received after Threshold is met.

---
+++ test/Seq.App.Thresholds.Tests/ThresholdReactorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Seq.App.Thresholds;
+
+using NUnit.Framework;
+using Moq;
+
+using Seq.App.Thresholds.Tests.Support;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+
+using Serilog;
+
+namespace Seq.App.Thresholds.Tests
+{
+    [TestFixture]
+    public class ThresholdReactorTests
+    {
+        private Mock<ILogger> _logger; 
+
+        [Test]
+        public void when_threshold_exceeded_only_one_message_should_be_logged()
+        {
+            // arrange
+            var sut = new ThresholdReactor()
+            {
+                EventsInWindowThreshold = 5, 
+                ThresholdName = Guid.NewGuid().ToString(), 
+                WindowSeconds = 20
+            };
+
+            _logger = new Mock<ILogger>();
+
+            var appHost = new Mock<IAppHost>();
+            appHost.SetupGet(h => h.Host).Returns(new Host(new[] { "localhost" }, "test"));
+            appHost.SetupGet(h => h.Logger).Returns(_logger.Object);
+
+            sut.Attach(appHost.Object);
+            
+            // act
+            SendEvents(sut, 7);
+
+            // assert
+            _logger.Verify(l => l.Information(It.IsAny<string>(), It.IsAny<object[]>()), Times.Once());
+        }
+
+        private static void SendEvents(ISubscribeTo<LogEventData> sut, int numberOfEvents)
+        {
+            for (var i = 0; i < numberOfEvents; i++)
+            {
+                sut.On(Some.LogEvent());
+            }
+        }
+    }
+}
