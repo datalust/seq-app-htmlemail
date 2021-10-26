f8b1ed99daea8d0cc8bbff03d15a6a151003f35c
Matt Nelson-White <mnelsonwhite@gmail.com>
2021-10-26 11:46:29 +1000

    Change: certificate validation to only fail on invalid certificates
    Avoid server name check which is not often implemented in STMP clients.

--- src/Seq.App.EmailPlus/DirectMailGateway.cs
+++ src/Seq.App.EmailPlus/DirectMailGateway.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using MailKit.Net.Smtp;
 using MimeKit;
@@ -11,13 +13,26 @@ namespace Seq.App.EmailPlus
         {
             if (message == null) throw new ArgumentNullException(nameof(message));
 
-            var client = new SmtpClient();
-                
+            var client = new SmtpClient
+            {
+                ServerCertificateValidationCallback = ServerCertificateValidation
+            };
+
             await client.ConnectAsync(options.Host, options.Port, options.SocketOptions);
             if (options.RequiresAuthentication)
                 await client.AuthenticateAsync(options.Username, options.Password);
             await client.SendAsync(message);
             await client.DisconnectAsync(true);
+        }
+
+        private static bool ServerCertificateValidation(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors errors)
+        {
+            var cert2 = new X509Certificate2(certificate);
+            return chain.Build(cert2);
         }
     }
 }
